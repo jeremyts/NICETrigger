d51acffae535bf85f3d2be42471da4f3004b7f32
Jeremy Saunders <jeremy@jhouseconsulting.com>
2023-06-06 19:07:23 +0800

    Add project files.

---
+++ NICETrigger/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace NICETrigger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelAction = new System.Windows.Forms.Label();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.timerToExit = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelAction
+            // 
+            this.labelAction.AutoSize = true;
+            this.labelAction.BackColor = System.Drawing.SystemColors.Control;
+            this.labelAction.Location = new System.Drawing.Point(12, 9);
+            this.labelAction.Name = "labelAction";
+            this.labelAction.Size = new System.Drawing.Size(37, 13);
+            this.labelAction.TabIndex = 0;
+            this.labelAction.Text = "Action";
+            this.labelAction.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.labelAction.Click += new System.EventHandler(this.labelAction_Click);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(57, 9);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(520, 20);
+            this.textBoxOutput.TabIndex = 1;
+            this.textBoxOutput.TextChanged += new System.EventHandler(this.textBoxOutput_TextChanged);
+            // 
+            // timerToExit
+            // 
+            this.timerToExit.Tick += new System.EventHandler(this.timerToExit_Tick);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(589, 36);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.labelAction);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "NICE Trigger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Label labelAction;
+        private System.Windows.Forms.Timer timerToExit;
+    }
+}
+
---
+++ NICETrigger/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NICETrigger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = true;
+            this.MinimizeBox = false;
+            this.MaximizeBox = false;
+            // Disable the close button('X')
+            this.ControlBox = false;
+            timerToExit.Interval = 5000;
+            timerToExit.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string txtoutput = "";
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length > 1)
+            {
+                txtoutput = txtoutput + String.Join(" ", args.Skip(1));
+            }
+            else
+            {
+                txtoutput = txtoutput + "no arguments provided";
+            }
+            textBoxOutput.Text = txtoutput;
+            textBoxOutput.Enabled = false;
+        }
+ 
+        private void textBoxOutput_TextChanged(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void timerToExit_Tick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void labelAction_Click(object sender, EventArgs e)
+        {
+            //
+        }
+    }
+}
